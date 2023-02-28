ea2b26164d4922cb8675f474206a8e02e67fe61a
Dron07Stm12 <dronperenen@gmail.com>
2023-02-28 20:08:11 +0200

    Компоненты ПО на основе класса добавляются в конвейер с помощью метода UseMiddleware()

--- Startup.cs
+++ Startup.cs
@@ -59,16 +59,16 @@ namespace Platform
             //});
 
 
-            Func<HttpContext, Func<Task>, Task> func = async delegate (HttpContext context, Func<Task> task)
-            {
-                if (context.Request.Method == HttpMethods.Get && context.Request.Query["myRequest"] == "true" )
-                {
-                    await context.Response.WriteAsync("myRequest\n");
-                }
-                await task(); 
-            };
+            //Func<HttpContext, Func<Task>, Task> func = async delegate (HttpContext context, Func<Task> task)
+            //{
+            //    if (context.Request.Method == HttpMethods.Get && context.Request.Query["myRequest"] == "true")
+            //    {
+            //        await context.Response.WriteAsync("myRequest\n");
+            //    }
+            //    await task();
+            //};
 
-            app.Use(func);
+            //app.Use(func);
 
             //Новое промежуточное ПО немедленно вызывает следующий метод для передачи запроса по конвейеру,
             //а затем использует метод WriteAsync. метод для добавления строки в тело ответа.
@@ -102,18 +102,18 @@ namespace Platform
 
             //метод Use регистрирует компонент промежуточного слоя(ПО) в методе Configure
             //для обработки его(компонента) через конвеер запросов
-            //app.Use(async (context, next) =>
-            //{
-            //    if (context.Request.Method == HttpMethods.Get
-            //    && context.Request.Query["custom"] == "true")
-            //    {
-            //        await context.Response.WriteAsync("Custom Middleware \n");
-            //    }
-            //    await next();
-            //});
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Method == HttpMethods.Get
+                && context.Request.Query["custom"] == "true")
+                {
+                    await context.Response.WriteAsync("Custom Middleware \n");
+                }
+                await next();
+            });
 
             //метод UseMiddleware регистрирует класс где находится  компонент ПО промежуточного слоя
-            //app.UseMiddleware<QueryStringMiddleWare>();
+            app.UseMiddleware<QueryStringMiddleWare>();
 
             app.UseRouting();
 
